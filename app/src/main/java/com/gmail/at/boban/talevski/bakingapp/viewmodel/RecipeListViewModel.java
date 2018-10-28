632859478a549518ac96aec620075f77616f10b2
bobtal <talevski.boban@gmail.com>
2018-10-28 12:16:18 +0100

    Implements Internet connectivity check before loading recipe list in MainActivity/RecipeListFragment

--- app/src/main/java/com/gmail/at/boban/talevski/bakingapp/viewmodel/RecipeListViewModel.java
+++ app/src/main/java/com/gmail/at/boban/talevski/bakingapp/viewmodel/RecipeListViewModel.java
@@ -1,15 +1,20 @@
 package com.gmail.at.boban.talevski.bakingapp.viewmodel;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.gmail.at.boban.talevski.bakingapp.R;
 import com.gmail.at.boban.talevski.bakingapp.api.UdacityRecipeApi;
 import com.gmail.at.boban.talevski.bakingapp.model.Ingredient;
 import com.gmail.at.boban.talevski.bakingapp.model.Recipe;
 import com.gmail.at.boban.talevski.bakingapp.model.Step;
 import com.gmail.at.boban.talevski.bakingapp.network.RetrofitClientInstance;
+import com.gmail.at.boban.talevski.bakingapp.utils.NetworkUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,12 +23,16 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class RecipeListViewModel extends ViewModel {
+public class RecipeListViewModel extends AndroidViewModel {
     private static final String TAG = RecipeListViewModel.class.getSimpleName();
 
     private MutableLiveData<List<Recipe>> recipeList;
     private MutableLiveData<List<Ingredient>> ingredientList;
     private MutableLiveData<List<Step>> stepList;
+
+    public RecipeListViewModel(@NonNull Application application) {
+        super(application);
+    }
 
 
     public LiveData<List<Recipe>> getRecipes() {
@@ -51,31 +60,33 @@ public class RecipeListViewModel extends ViewModel {
     }
 
     private void loadRecipes() {
-        final MutableLiveData<List<Recipe>> results = new MutableLiveData<>();
+        if (NetworkUtils.isNetworkAvailable(getApplication())) {
+            final MutableLiveData<List<Recipe>> results = new MutableLiveData<>();
 
-        RetrofitClientInstance.getRetrofitInstance().create(UdacityRecipeApi.class).getRecipes()
-                .enqueue(new Callback<List<Recipe>>() {
-                    @Override
-                    public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-                        Log.d(TAG, "response received");
-                        if (response.body() != null) {
-                            results.setValue(response.body());
-                            recipeList.setValue(results.getValue());
+            RetrofitClientInstance.getRetrofitInstance().create(UdacityRecipeApi.class).getRecipes()
+                    .enqueue(new Callback<List<Recipe>>() {
+                        @Override
+                        public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                            Log.d(TAG, "response received");
+                            if (response.body() != null) {
+                                results.setValue(response.body());
+                                recipeList.setValue(results.getValue());
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                        Log.d(TAG, "response failure");
-                        if (t instanceof IOException) {
-                            Log.d(TAG, "this is an actual network failure :( inform the user and possibly retry");
+                        @Override
+                        public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                            Log.d(TAG, "response failure");
+                            if (t instanceof IOException) {
+                                Log.d(TAG, "this is an actual network failure :( inform the user and possibly retry");
+                            } else {
+                                Log.d(TAG, "Probably conversion error - message: " + t.getMessage());
+                            }
                         }
-                        else {
-                            Log.d(TAG, "Probably conversion error - message: " + t.getMessage() );
-                        }
-                    }
-                });
-
+                    });
+        } else {
+            Toast.makeText(getApplication(), R.string.no_internet, Toast.LENGTH_SHORT).show();
+        }
     }
 
 
