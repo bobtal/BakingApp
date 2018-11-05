54b5a6bb1adbaaeacf885e8a687c743c2f7c6a59
bobtal <talevski.boban@gmail.com>
2018-11-05 01:51:27 +0100

    Adds a basic widget implementation which shows a static image and launches the app on click of the image using a pending intent. The same image is also added as a launcher icon.

---
+++ app/src/main/java/com/gmail/at/boban/talevski/bakingapp/widget/BakingWidgetProvider.java
@@ -0,0 +1,53 @@
+package com.gmail.at.boban.talevski.bakingapp.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.gmail.at.boban.talevski.bakingapp.R;
+import com.gmail.at.boban.talevski.bakingapp.ui.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class BakingWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
+
+        // Create an Intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Widgets allow click handlers only to launch pending intents
+        views.setOnClickPendingIntent(R.id.widget_baking_image, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
