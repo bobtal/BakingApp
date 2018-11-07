a39675b34dddae8cfee9e29cd796266e334be59b
bobtal <talevski.boban@gmail.com>
2018-11-07 02:42:15 +0100

    Adds logic to preserve video player state (playing/paused and playback position) in RecipeStepDetailsFragment. Implements a hack of sorts to prevent unexpected behavior of observer's onChanged method being triggered more often than expected, i.e. on initialization and device rotation even when the step wasn't actually changed.

--- app/src/main/java/com/gmail/at/boban/talevski/bakingapp/ui/RecipeStepDetailsFragment.java
+++ app/src/main/java/com/gmail/at/boban/talevski/bakingapp/ui/RecipeStepDetailsFragment.java
@@ -25,12 +25,26 @@ import com.google.android.exoplayer2.util.Util;
 
 public class RecipeStepDetailsFragment extends Fragment {
 
+    private static final String EXTRA_PLAYER_IS_PLAYING = "EXTRA_PLAYER_IS_PLAYING";
+    private static final String EXTRA_CURRENT_PLAYER_POSITION = "EXTRA_CURRENT_PLAYER_POSITION";
+    private static final String EXTRA_CURRENT_STEP = "EXTRA_CURRENT_STEP";
+
     private RecipeStepDetailsViewModel detailsViewModel;
     private PlayerView playerView;
     private TextView stepInstructions;
     private SimpleExoPlayer player;
 
-    private boolean landscape;
+    // variables to keep player state
+    private boolean isPlaying;
+    private long currentPlayerPosition;
+    // a "hack variable" to prevent observer's onChanged method from executing
+    // if there wasn't actually a change in the step
+    // it executes on initialization and/or rotation "for no apparent reason"
+    // which caused the currentPlayerPosition to be reset to 0 even if we didn't change the step
+    // and only rotated the device.
+    // Debate on whether this behavior is a bug or it's by design
+    // https://stackoverflow.com/questions/50236778/why-livedata-observer-is-being-triggered-twice-for-a-newly-attached-observer
+    private int currentStep;
 
     // Mandatory empty constructor
     public RecipeStepDetailsFragment() {}
@@ -52,14 +66,47 @@ public class RecipeStepDetailsFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(EXTRA_PLAYER_IS_PLAYING)
+                && savedInstanceState.containsKey(EXTRA_CURRENT_PLAYER_POSITION)
+                && savedInstanceState.containsKey(EXTRA_CURRENT_STEP)) {
+            // setup the state of the player if there are saved values in savedInstanceState
+            currentStep = savedInstanceState.getInt(EXTRA_CURRENT_STEP);
+            isPlaying = savedInstanceState.getBoolean(EXTRA_PLAYER_IS_PLAYING);
+            currentPlayerPosition = savedInstanceState.getLong(EXTRA_CURRENT_PLAYER_POSITION);
+        } else {
+            // set defaults if there are not, player should start playing from the beginning
+            currentStep = 0;
+            isPlaying = true;
+            currentPlayerPosition = 0;
+        }
+
         detailsViewModel = ViewModelProviders.of(getActivity()).get(RecipeStepDetailsViewModel.class);
         detailsViewModel.getStepPosition().observe(getActivity(), new Observer<Integer>() {
             @Override
-            public void onChanged(@Nullable Integer integer) {
-                releasePlayer(false);
-                setInstructionText();
-                initializePlayer();
-                setupPlayerMediaSource();
+            public void onChanged(@Nullable Integer newStep) {
+                if (currentStep == newStep) {
+                    // do nothing if there's no change in the step and
+                    // onChanged is triggered on initialization
+                } else {
+                    // do the stuff supposed to be done on actual change in the step
+
+                    // stop the player
+                    playerView.getPlayer().stop();
+
+                    // update the step instructions using the new step instructions from the viewmodel
+                    setInstructionText();
+
+                    // we want the player to start playing from the beginning of the video
+                    // automatically on switching steps
+                    isPlaying = true;
+                    currentPlayerPosition = 0;
+                    setupPlayerMediaSource();
+
+                    // set the current step to the new one
+                    // to keep the hack working
+                    currentStep = newStep;
+                }
             }
         });
 
@@ -90,10 +137,28 @@ public class RecipeStepDetailsFragment extends Fragment {
         // Prepare the player with the source.
         player.prepare(videoSource);
 
+        // seek to the specified position
+        player.seekTo(currentPlayerPosition);
+
         // Start playing the sample
-        player.setPlayWhenReady(true);
+        player.setPlayWhenReady(isPlaying);
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save the state in the outState
+        boolean isPlaying = playerView.getPlayer().getPlayWhenReady();
+        long currentPlayerPosition = playerView.getPlayer().getCurrentPosition();
+        int currentStep = detailsViewModel.getStepPosition().getValue();
+        outState.putBoolean(EXTRA_PLAYER_IS_PLAYING, isPlaying);
+        outState.putLong(EXTRA_CURRENT_PLAYER_POSITION, currentPlayerPosition);
+        outState.putInt(EXTRA_CURRENT_STEP, currentStep);
+
+        // pause the player
+        playerView.getPlayer().setPlayWhenReady(false);
+    }
 
     @Override
     public void onDestroyView() {
